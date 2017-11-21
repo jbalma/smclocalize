a2eab767a5667f35d7bb2161fb8a043028cfee9d
Minderwinter <charles.midwinter@gmail.com>
2017-11-21 11:35:58 -0600

    More progress

--- localization_script/class_rough.py
+++ localization_script/class_rough.py
@@ -84,13 +84,20 @@ class SensorModel:
         self.num_moving_sensors, self.num_stationary_sensors, self.num_sensors, self.num_dimensions, self.num_x_vars, self.num_y_discrete_vars, self.num_y_continuous_vars, self.num_ping_statuses = self.numbers
         
         self.room_size = np.array([room_width, room_height])
-
-    def sample_x_initial(self, num_samples = 1, stationary_position_guess_error = 1.0):
+        
         self.stationary_sensor_position_guesses = self.room_size*np.array([[0.5, 1.0],
                                                                            [0.5, 0.0],
                                                                            [0.0, 0.5],
                                                                            [1.0, 0.5]])[0:self.num_stationary_sensors]
         
+        self.stationary_position_guess_error = 1.0
+        
+        self.receive_probability_zero_distance = 1.0 # Value from real data is approximately 1.0
+        self.receive_probability_reference_distance = 0.7 #Value from real data is approximately 0.7
+        self.reference_distance = 20.0 # Value from real data is approximately 20.0
+        self.scale_factor = reference_distance/np.log(receive_probability_reference_distance/receive_probability_zero_distance)
+
+    def sample_x_initial(self, num_samples = 1):
         if self.num_moving_sensors > 0:
             moving_sensors = np.random.uniform(high=np.tile(self.room_size, self.num_moving_sensors),
                                                size=(num_samples, self.num_moving_sensors*self.num_dimensions))
@@ -98,14 +105,14 @@ class SensorModel:
             moving_sensors = np.array([])
         if self.num_stationary_sensors > 0:
             stationary_sensors = np.random.normal(loc=self.stationary_sensor_position_guesses.flatten(),
-                                                  scale=stationary_position_guess_error,
+                                                  scale=self.stationary_position_guess_error,
                                                   size=(num_samples, self.num_stationary_sensors*self.num_dimensions))
         else:
             stationary_sensors = np.array([])
         return np.squeeze(np.concatenate((moving_sensors, stationary_sensors), axis=1))
     
-    def plot_x_initial_samples(self, num_samples = 1, stationary_position_guess_error = 1.0):
-        x_initial_samples = self.sample_x_initial(num_samples, stationary_position_guess_error)
+    def plot_x_initial_samples(self, num_samples = 1):
+        x_initial_samples = self.sample_x_initial(num_samples)
         for sensor_index in range(self.num_sensors):
             plt.plot(x_initial_samples[:,sensor_index*2],
                      x_initial_samples[:,sensor_index*2 + 1],
@@ -122,16 +129,59 @@ class SensorModel:
                                            fill=False,
                                            color='green',
                                            label='Room boundary'))
-            plt.xlim(0 - stationary_position_guess_error,
-                     self.room_size[0] + stationary_position_guess_error)
-            plt.ylim(0 - stationary_position_guess_error,
-                     self.room_size[1] + stationary_position_guess_error)
+            plt.xlim(0 - self.stationary_position_guess_error,
+                     self.room_size[0] + self.stationary_position_guess_error)
+            plt.ylim(0 - self.stationary_position_guess_error,
+                     self.room_size[1] + self.stationary_position_guess_error)
             plt.legend(loc='upper left', bbox_to_anchor=(1.0, 1.0))
             plt.show()
+    
+    def sample_x_bar_x_prev(self, x_prev, moving_sensor_drift = 0.5, stationary_sensor_drift = 0.25):
+        if self.num_moving_sensors > 0:
+            x_prev_moving_sensors = x_prev[...,:self.num_moving_sensors*self.num_dimensions]
+            moving_sensor_positions = np.random.normal(loc=x_prev_moving_sensors,
+                                                       scale=moving_sensor_drift)
+        else:
+            moving_sensor_positions = np.array([])
+        if self.num_stationary_sensors > 0:
+            x_prev_stationary_sensors = x_prev[...,self.num_moving_sensors*self.num_dimensions:]
+            position_guesses = np.random.normal(loc=np.tile(self.stationary_sensor_position_guesses.flatten(),
+                                                            x_prev_stationary_sensors.shape[:-1] + (1,)),
+                                                scale=self.stationary_sensor_position_guess_error)
+            drifted_positions = np.random.normal(loc=x_prev_stationary_sensors,
+                                                 scale=stationary_sensor_drift)
+            stationary_sensor_positions = (position_guesses + drifted_positions)/2
+        else:
+            stationary_sensor_positions = np.array([])
+        return np.concatenate((moving_sensor_positions, stationary_sensor_positions), axis=-1)
+
+    def distances(self, x):
+        if x.ndim == 1:
+            x = np.expand_dims(x, 0)
+        num_x_values = x.shape[0]
+        return np.squeeze(np.delete(np.linalg.norm(np.subtract(np.tile(x.reshape((num_x_values, self.num_sensors, self.num_dimensions)),
+                                                                       (1, self.num_sensors, 1)),
+                np.repeat(x.reshape((self.num_x_values,
+                                     self.num_sensors,
+                                     self.num_dimensions)),
+                self.num_sensors,
+                axis = 1)),
+                axis=2),
+                np.arange(self.num_sensors)*self.num_sensors + np.arange(self.num_sensors),1))
+
+    def ping_status_probabilities(self, distance):
+        receive_probability=self.receive_probability_zero_distance*np.exp(distance/self.scale_factor)
+        return np.stack((receive_probability, 1 - receive_probability), axis=0)
+
 
 
 test_model = SensorModel(3, 4, 20.0, 10.0)
 
-x_initial_samples = test_model.sample_x_initial(1000, 1.0)
+x_initial_samples = test_model.sample_x_initial(1000)
 
-test_model.plot_x_initial_samples(1000, 1.0)
+test_model.plot_x_initial_samples(1000)
+
+test_x_value = test_model.sample_x_initial()
+
+test_model.sample_x_bar_x_prev(np.tile(test_x_value, (1000, 1)))
+
