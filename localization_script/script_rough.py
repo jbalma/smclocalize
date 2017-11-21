0086e2b78dab0b6b571abd8c06fce3deb7195b92
Minderwinter <charles.midwinter@gmail.com>
2017-11-20 20:59:27 -0600

    Defining variables

--- localization_script/script_rough.py
+++ localization_script/script_rough.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+This script defines a function that takes in sensor data & a set of previously estimated sensor positions, and output a new set of estimated positions for the sensors.
 
-This is a temporary script file.
+This initial version is a simple adaptation of the code Ted created in the notebook bayes_filter_sim_rssi.ipynb.
 """
 
 import math
@@ -13,3 +13,20 @@ from scipy import misc
 from scipy import stats
 import matplotlib.pyplot as plt
 from matplotlib import patches
+
+#This is right from Ted's code. Simply initializing the number of sensors.
+num_moving_sensors = 3
+num_stationary_sensors = 4 # Currently must be 4 or fewer because we manually place them from a fixed list of 4 positions
+
+#Initialize sensor_names to empty array.
+sensor_names = []
+
+#Self explanatory, but one thing to note is that arrays start with an index of
+#zero in Python, rather than 1 in R. Hence the "+1" below.
+for moving_sensor_index in range(num_moving_sensors):
+    sensor_names.append('Moving sensor {}'.format(moving_sensor_index + 1))
+    
+for stationary_sensor_index in range(num_stationary_sensors):
+    sensor_names.append('Stationary sensor {}'.format(stationary_sensor_index + 1))
+
+num_sensors = len(sensor_names)
