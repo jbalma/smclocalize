c9fef59aebe9b15de7554299f08459d5c5ac3ddd
Minderwinter <charles.midwinter@gmail.com>
2017-11-20 20:19:11 -0600

    Initial commit
    Description of file and initial imports

---
+++ localization_script/script_rough.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import math
+import time
+import sys
+import numpy as np
+from scipy import misc
+from scipy import stats
+import matplotlib.pyplot as plt
+from matplotlib import patches
