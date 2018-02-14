338e7a54d206355663db06dafc8e682aaac2d8ea
Theodore Quinn <tcquinn@tcquinn.org>
2018-02-14 16:27:52 -0600

    Add . to names of modules so that Jupyter can find them

--- smclocalize/__init__.py
+++ smclocalize/__init__.py
@@ -1,3 +1,3 @@
-from sensor_model import *
-from sensor_variable_structure import *
-from smc_model import *
+from .sensor_model import *
+from .sensor_variable_structure import *
+from .smc_model import *
